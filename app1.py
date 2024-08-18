6c09984f27561495b6de0d8d49e7575972e6b4d2
Mohamad anas fattoum <samaanas30@gmail.com>
2024-08-18 16:17:11 +0200

    add 2 defs students mit und ohne id

--- app1.py
+++ app1.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = [
+    {'name':'anas','age':30,'depatment': 'IT'},
+    {'name':'Max', 'age':40,'depatment':'CH'},
+    {'name':'Ali', 'age':50,'depatment':'MA'}]
 
 @app.get("/root")
 
 def root():
     return{'message':'hello'}
+
+
+
+@app.get("/students")
+def get_all_students():
+    return students
+
+@app.get("/students/{id}")
+def get_student_by_id(id:int):
+    return students[id]
